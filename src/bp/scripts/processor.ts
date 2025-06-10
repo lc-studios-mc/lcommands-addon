5ddecb3b904823ef41c26075aa3e1a42db6ebbeb
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-10 16:18:22 +0900

    add quick mode functionality

--- src/bp/scripts/processor.ts
+++ src/bp/scripts/processor.ts
@@ -1,6 +1,8 @@
 import * as mc from "@minecraft/server";
-import { profilesReadonly } from "./profile_registry";
+import { profilesByNameReadonly, profilesReadonly } from "./profile_registry";
 import type { LCommandProfile } from "./types";
+
+const playerQuickModeFlags = new Map<mc.Player, boolean>();
 
 function handleCustomCommand(
 	profile: LCommandProfile,
@@ -45,5 +47,56 @@ mc.system.beforeEvents.startup.subscribe((e) => {
 				},
 			);
 		}
+	}
+
+	e.customCommandRegistry.registerCommand(
+		{
+			name: `l:quick_mode`,
+			description: "Eliminates the need to type '/l:' first at the beginning of an LCommand statement.",
+			permissionLevel: mc.CommandPermissionLevel.Any,
+		},
+		(origin: mc.CustomCommandOrigin) => {
+			const source = origin.sourceEntity;
+			if (!(source && source instanceof mc.Player))
+				return {
+					status: mc.CustomCommandStatus.Failure,
+					message: "Only a player can run this command.",
+				};
+
+			const isActive = playerQuickModeFlags.get(source) === true;
+
+			playerQuickModeFlags.set(source, !isActive);
+
+			if (isActive) {
+				return {
+					status: mc.CustomCommandStatus.Success,
+					message: "Turned OFF quick mode.",
+				};
+			}
+
+			return {
+				status: mc.CustomCommandStatus.Success,
+				message: "Turned ON quick mode. It will be reset on world exit.",
+			};
+		},
+	);
+});
+
+mc.world.beforeEvents.chatSend.subscribe((e) => {
+	const isQuickModeActive = playerQuickModeFlags.get(e.sender) === true;
+	if (!isQuickModeActive) return;
+
+	const profile = profilesByNameReadonly.get(e.message);
+	if (!profile) return;
+
+	e.cancel = true;
+
+	try {
+		profile.callback({
+			source: e.sender,
+			params: [],
+		});
+	} catch (error) {
+		e.sender.sendMessage(`§cFailed to run a LCommand: ${error}`);
 	}
 });
