c749d6af1568f7eec44ea03f774be6c3ce53f03b
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-10 16:25:17 +0900

    add lcommand to toggle nv

--- src/bp/scripts/register_entries.ts
+++ src/bp/scripts/register_entries.ts
@@ -1,1 +1,16 @@
 import { registerProfile } from "./profile_registry";
+
+registerProfile({
+	names: ["v", "vv", "v1"],
+	description: "Toggles the night vision effect.",
+	callback(arg) {
+		const currentNv = arg.source.getEffect("night_vision");
+
+		if (currentNv) {
+			arg.source.removeEffect("night_vision");
+			return;
+		}
+
+		arg.source.runCommand("effect @s night_vision infinite 1 true"); // infinite duration is only available via command
+	},
+});
